11320e3ccd48472a7ea8f50cad3e5fe1a9874994
bimbo-ade <bimboadeyemi65@gmail.com>
2023-03-08 10:34:29 +0100

    initialised firebase

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+import { initializeApp } from "firebase/app";
+const firebaseConfig = {
+  apiKey: "AIzaSyA6S8z51Tg_LOFDYgcznvG9VJkxDpKKwmA",
+  authDomain: "foodlang-auth.firebaseapp.com",
+  projectId: "foodlang-auth",
+  storageBucket: "foodlang-auth.appspot.com",
+  messagingSenderId: "776732177726",
+  appId: "1:776732177726:web:1a51825d4478ab2abb286f",
+};
+
+const app = initializeApp(firebaseConfig);
