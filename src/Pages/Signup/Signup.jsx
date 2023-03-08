5bad24653370b29ca0d7dd2061922b3c3ac36b22
bimbo-ade <bimboadeyemi65@gmail.com>
2023-03-08 11:11:36 +0100

    modification to signup page

--- src/Pages/Signup/Signup.jsx
+++ src/Pages/Signup/Signup.jsx
@@ -1,8 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import { Div } from "./Signup.style";
 import { Link } from "react-router-dom";
 import Images from "../../assests/images";
+import { auth } from "firebase/auth";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+
 const Signup = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const signup = () => {};
+
   return (
     <Div>
       <img src={Images.banner} alt="login" width={900} />
@@ -10,10 +18,18 @@ const Signup = () => {
 
       <div className="cont">
         <h2>Sign Up</h2>
-        <input type="text" placeholder="fullname" />
-        <input type="text" placeholder="email" />
 
-        <input type="password" placeholder="password" />
+        <input
+          type="text"
+          placeholder="email"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+
+        <input
+          type="password"
+          placeholder="password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
         <button>Submit</button>
         <p>
           Already have an Account?{" "}
